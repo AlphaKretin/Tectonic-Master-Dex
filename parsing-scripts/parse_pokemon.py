97cbdaf32e95f43c1568e65288da0eb4f0405def
AlphaKretin <deewhydeeecks@gmail.com>
2024-03-18 08:25:58 +1030

    Fix linter

--- parsing-scripts/parse_pokemon.py
+++ parsing-scripts/parse_pokemon.py
@@ -1,17 +1,27 @@
 import re
 
 
+# Index
+def parse_pokedex_number(mon_text):
+    num_match = re.search(r"^\[(\d+)\]", mon_text, re.MULTILINE)
+    num_raw = num_match.group(1)
+    num = int(num_raw)
+    return num
+
+
 # Name
 def parse_pokemon_name(mon_text):
     name_match = re.search(r"^Name = (.+)", mon_text, re.MULTILINE)
     name = name_match.group(1)
     return name
 
+
 # InternalName
 def parse_internal_name(mon_text):
     id_match = re.search(r"^InternalName = (.+)", mon_text, re.MULTILINE)
     id = id_match.group(1)
     return id
+
 
 # Type1
 # Type2
@@ -26,8 +36,9 @@ def parse_types(mon_text):
     if type2_match:
         type2 = type2_match.group(1).title()
         types.append(type2)
-    
+
     return types
+
 
 # BaseStats
 def parse_base_stats(mon_text):
@@ -41,9 +52,10 @@ def parse_base_stats(mon_text):
         "defense": stats_nums[2],
         "speed": stats_nums[3],
         "sp_atk": stats_nums[4],
-        "sp_def": stats_nums[5]
+        "sp_def": stats_nums[5],
     }
     return stats
+
 
 # GenderRate
 # Adapted from Plugins/Tectonic Master Dex/PokeDex Entry/PokemonPokedexInfo_Scene.rb
@@ -55,14 +67,16 @@ gender_rate_map = {
     "Female75Percent": "3/4 Fem.",
     "FemaleSevenEighths": "7/8 Fem.",
     "AlwaysFemale": "Female",
-    "Genderless": "None"
+    "Genderless": "None",
 }
+
 
 def parse_gender_rate(mon_text):
     rate_match = re.search(r"^GenderRate = (.+)", mon_text, re.MULTILINE)
     rate_raw = rate_match.group(1)
     rate = gender_rate_map[rate_raw]
     return rate
+
 
 # GrowthRate
 # Some names are abbreviated in-game, their full canon names are used here
@@ -72,14 +86,16 @@ growth_rate_map = {
     "Fluctuating": "Fluctuating",
     "Parabolic": "Medium Slow",
     "Fast": "Fast",
-    "Slow": "Slow"
+    "Slow": "Slow",
 }
+
 
 def parse_growth_rate(mon_text):
     rate_match = re.search(r"^GrowthRate = (.+)", mon_text, re.MULTILINE)
     rate_raw = rate_match.group(1)
     rate = growth_rate_map[rate_raw]
     return rate
+
 
 # BaseEXP
 def parse_base_exp(mon_text):
@@ -88,6 +104,7 @@ def parse_base_exp(mon_text):
     exp = int(exp_raw)
     return exp
 
+
 # Rareness
 def parse_catch_rate(mon_text):
     rate_match = re.search(r"^Rareness = (.+)", mon_text, re.MULTILINE)
@@ -95,12 +112,14 @@ def parse_catch_rate(mon_text):
     rate = int(rate_raw)
     return rate
 
+
 # Happiness
 def parse_base_happiness(mon_text):
     happiness_match = re.search(r"^Happiness = (.+)", mon_text, re.MULTILINE)
     happiness_raw = happiness_match.group(1)
     happiness = int(happiness_raw)
     return happiness
+
 
 # Abilities
 # Moves
